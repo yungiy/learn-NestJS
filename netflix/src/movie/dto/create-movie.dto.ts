03c7e09a86e0f6c312d0470a6661c0a3c0b40b71
yungiy <devyungiy@gmail.com>
2025-01-06 15:35:58 +0900

    [update]
    - class validator 정리
    - custom validator
    - validation Pipe

--- netflix/src/movie/dto/create-movie.dto.ts
+++ netflix/src/movie/dto/create-movie.dto.ts
@@ -1,8 +1,68 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  Contains,
+  IsAlphanumeric,
+  IsCreditCard,
+  IsEmpty,
+  IsIn,
+  IsLatLong,
+  IsNotEmpty,
+  IsNotIn,
+  IsOptional,
+  MinLength,
+  NotContains,
+  Validate,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
 
 export class CreateMovieDto {
+  @IsOptional()
   @IsNotEmpty()
   title: string;
+
   @IsNotEmpty()
   genre: string;
+
+  // 기본 validator
+  // null || undefined
+  // @IsDefined()
+  // @IsOptional()
+  // @Equals('yungiy')
+  // @NotEquals('yungiy')
+  // null || undefined || ''
+  // @IsEmpty()
+  // @IsNotEmpty()
+
+  // 타입 validator
+  // Array
+  // @IsIn(['action', 'fantasy'])
+  // @IsNotIn(['action', 'fantasy'])
+  // Boolean
+  // @IsBoolean
+  // String
+  // @IsString()
+  // Int
+  // @ISInt
+  // IsArray() array의 유무 판단
+  //@IsEnum()
+
+  // 숫자 validator
+  // @IsDivisible(5) number에 나눌 수 있는 수를 넣음
+  // @IsNegative()
+  // @Max()
+  // @Min()
+
+  // 문자 validator
+  //@Contains('yungiy')
+  // @NotContains('yungiy')
+
+  // @IsAlphanumeric() 문자와 숫자로 이루어져 있는가?
+  // @IsCreditCard() 실제로 존재할 수 있는 카드의 숫자가 들어왔는가?
+  // @IsHexColor() 16진수 판별
+  // @MaxLength(16)
+  // @MinLength(4)
+  // @IsUUID()
+  // @IsLatLong() // 위도 경도
 }
--- netflix/src/movie/dto/update-movie.dto.ts
+++ netflix/src/movie/dto/update-movie.dto.ts
@@ -1,11 +1,47 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  registerDecorator,
+  ValidateByOptions,
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+// custom validator
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    // 비밀 번호 길이는 4-8
+    return value.length > 4 && value.length < 8;
+  }
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return '비밀번호의 길이는 4~8자 여야합니다. 입력값 : ($value)';
+  }
+}
+
+function IsPasswordValid(validationOptions?: ValidateByOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      validator: PasswordValidator,
+    });
+  };
+}
 
 export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
   title?: string;
-  
+
   @IsNotEmpty()
   @IsOptional()
   genre?: string;
+
+  @IsPasswordValid()
+  test: string;
 }
