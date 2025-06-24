aa59d2e29f0334e5dfda0780e420548647eea76a
yungiy <devyungiy@gmail.com>
2025-06-25 01:16:25 +0900

    feat: crud
    - crud 생성
    - param, body 활용
    - Query 파라미터로 고도화

--- netflix/src/app.controller.ts
+++ netflix/src/app.controller.ts
@@ -1,65 +1,103 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Movie {
+  id: number;
+  title: string;
+  //character: string[];
+}
+
+// 무비에 대한 api 정의 @Controller('movie')
+@Controller('movie')
 export class AppController {
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+      //character: ['해리포터', '엠마왓슨'],
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+      //character: ['프로도', '간달프', '아라곤', '레골라스'],
+    },
+    {
+      id: 3,
+      title: '어벤져스',
+      //character: ['아이언맨', '캡틴 아메리카', '토르', '헐크'],
+    },
+    {
+      id: 4,
+      title: '원피스',
+      //character: ['루피', '조로', '나미', '상디'],
+    },
+  ];
+
+  private idCounter = 5;
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getMovies() {
-    return [
-      {
-        id: 1,
-        name: '해리포터',
-        character: ['해리포터', '엠마왓슨'],
-      },
-      {
-        id: 2,
-        name: '반지의 제왕',
-        character: ['프로도', '간달프', '아라곤', '레골라스'],
-      },
-      {
-        id: 3,
-        name: '어벤져스',
-        character: ['아이언맨', '캡틴 아메리카', '토르', '헐크'],
-      },
-      {
-        id: 4,
-        name: '원피스',
-        character: ['루피', '조로', '나미', '상디'],
-      },
-    ];
+  getMovies(@Query() title: string) {
+    if (!title) {
+      return this.movies;
+    }
+    return this.movies.filter((m) => m.title.startsWith(title));
   }
 
-  @Get()
-  getMovie() {
-    return {
-      id: 1,
-      name: '해리포터',
-      character: ['해리포터', '엠마왓슨'],
-    };
+  // 변수를 불러옴 @Param('변수')
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    const movie = this.movies.find((m) => m.id === +id);
+
+    if (!movie) {
+      throw new NotFoundException('없는 영화 id 값임!');
+    }
   }
 
   @Post()
-  postMovie() {
-    return {
-      id: 5,
-      name: '나루토',
-      character: ['나루토', '사스케', '사쿠라', '카카시'],
+  postMovie(@Body('title') title: string) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title,
     };
+    this.movies.push(movie);
+
+    return movie;
   }
 
-  @Patch()
-  patchMovie() {
-    return {
-      id: 4,
-      name: '원피스',
-      character: ['루피', '조로', '나미', '샹디'],
-    };
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body('title') title: string) {
+    const movie = this.movies.find((m) => m.id == +id);
+
+    if (!movie) {
+      throw new NotFoundException('없는 영화 id 값임!');
+    }
+
+    Object.assign(movie, { title });
+
+    return movie;
   }
 
-  @Delete()
-  deleteMovie() {
-    return 5
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    const movieIndex = this.movies.findIndex((m) => m.id == +id);
+
+    if (!movieIndex) {
+      throw new NotFoundException('없는 영화 id 값임!');
+    }
+
+    this.movies.splice(movieIndex, 1);
+    return id;
   }
 }
